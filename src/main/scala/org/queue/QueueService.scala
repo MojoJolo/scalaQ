f1a354ab4fa9c64c0be453cf26a3670a71d4d7d3
Jolo Balbin <jolo@jpbalb.in>
2013-08-13 22:56:45 +0800

    added error handling and output

--- src/main/scala/org/queue/QueueService.scala
+++ src/main/scala/org/queue/QueueService.scala
@@ -7,28 +7,46 @@ import HttpResponseStatus._
 import com.twitter.util.Future
 import scala.collection.mutable.Queue
 import org.jboss.netty.handler.codec.http.multipart._
+import org.jboss.netty.buffer.ChannelBuffers
+import org.jboss.netty.util.CharsetUtil
 
 class QueueService extends Service[HttpRequest, HttpResponse] {
   val q = Queue[String]()
-  
+
   def apply(request: HttpRequest) = {
     request.getUri match {
       case "/post" => post(request)
       case "/get" => get
-      case x => println(x)
+      case x => Future(new DefaultHttpResponse(HTTP_1_1, OK))
     }
+  }
 
-    Future(new DefaultHttpResponse(HTTP_1_1, OK))
-  }
-  
-  def post(request: HttpRequest) = if(request.getMethod == HttpMethod.POST) {
-    val decoder = new HttpPostRequestDecoder(new DefaultHttpDataFactory(false), request)
-    val value = decoder.getBodyHttpData("message").asInstanceOf[Attribute].getValue
-    
-    q.enqueue(value)
-  }
-  
-  def get {
-    println(q.dequeue)
+  def post(request: HttpRequest) = if (request.getMethod == HttpMethod.POST) {
+    try {
+      val decoder = new HttpPostRequestDecoder(new DefaultHttpDataFactory(false), request)
+      val value = decoder.getBodyHttpData("message").asInstanceOf[Attribute].getValue
+
+      q.enqueue(value)
+      Future(new DefaultHttpResponse(HTTP_1_1, OK))
+    } catch {
+      case e: Exception => Future(new DefaultHttpResponse(HTTP_1_1, INTERNAL_SERVER_ERROR))
+    }
+  } else Future(new DefaultHttpResponse(HTTP_1_1, METHOD_NOT_ALLOWED))
+
+  def get = {
+    try {
+      val resp = new DefaultHttpResponse(HTTP_1_1, OK)
+      resp.setHeader(HttpHeaders.Names.CONTENT_TYPE, "application/json")
+      resp.setContent(ChannelBuffers.copiedBuffer(q.dequeue, CharsetUtil.UTF_8))
+
+      Future(resp)
+    } catch {
+      case e: NoSuchElementException => {
+        val resp = new DefaultHttpResponse(HTTP_1_1, INTERNAL_SERVER_ERROR)
+        resp.setHeader(HttpHeaders.Names.CONTENT_TYPE, "application/json")
+        resp.setContent(ChannelBuffers.copiedBuffer("""{"error": "Queue is empty"}""", CharsetUtil.UTF_8))
+        Future(resp)
+      }
+    }
   }
 }
